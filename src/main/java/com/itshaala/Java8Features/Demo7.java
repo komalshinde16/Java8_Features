1bbb2d1369934f1adf2bdc9d02c7b2a67809c37f
komalshinde16 <komalambadasshinde@gmail.com>
2026-02-20 11:53:12 +0530

    demo7

---
+++ src/main/java/com/itshaala/Java8Features/Demo7.java
@@ -0,0 +1,47 @@
+package com.itshaala.Java8Features;
+
+public class Demo7 {
+    public static void main(String[] args) {
+        interface Interface1 {
+            void method1();
+
+            default void display() {
+                System.out.println("Interface1.display");
+            }
+        }
+
+        interface Interface2 {
+            void method2();
+
+            default void display() {
+                System.out.println("Interface2.display");
+            }
+        }
+
+        class A implements Interface1, Interface2 {
+
+            @Override
+            public void method1() {
+                System.out.println("Method 1 overridden in A");
+            }
+
+            @Override
+            public void method2() {
+                System.out.println("Method 2 overridden in A");
+            }
+
+            @Override
+            public void display() {
+                //new logic
+                Interface1.super.display();
+                Interface2.super.display();
+            }
+        }
+
+
+        A a = new A();
+        a.method1();
+        a.method2();
+        a.display();
+    }
+}
